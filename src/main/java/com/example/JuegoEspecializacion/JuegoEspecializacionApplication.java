f2b70d0f318a950aef1bb2bf67cfe774c68c2aa9
Daniel Alejandro <d.toro05@ufromail.cl>
2024-07-04 02:44:23 -0400

    Base proyect

---
+++ src/main/java/com/example/JuegoEspecializacion/JuegoEspecializacionApplication.java
@@ -0,0 +1,13 @@
+package com.example.JuegoEspecializacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoEspecializacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoEspecializacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JuegoEspecializacion/JuegoEspecializacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JuegoEspecializacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuegoEspecializacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
